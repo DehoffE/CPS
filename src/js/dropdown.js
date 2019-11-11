79a2500791d6e184590d221f6603370343b8bb0d
DehoffE <54005436+DehoffE@users.noreply.github.com>
2019-11-11 05:16:12 +0300

    tablet-version

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,12 +1,21 @@
 let burger = document.querySelector('.burger');
-let dropdown = document.querySelector('.dropdown-menu');
+let dropdownWrap = document.querySelector('.dropdown-menu-wrap');
+let dropdown = dropdownWrap.querySelector('.dropdown-menu');
 let close = dropdown.querySelector('.dropdown-menu__close');
 
 burger.addEventListener('click', function() {
-    dropdown.classList.add('dropdown-menu--open');
+    dropdownWrap.classList.add('dropdown-menu-wrap--open');
+
+    setTimeout(function(){
+        dropdown.classList.add('dropdown-menu--open');
+    }, 10);
 });
 
 close.addEventListener('click', function() {
     dropdown.classList.remove('dropdown-menu--open');
+
+    setTimeout(function(){
+        dropdownWrap.classList.remove('dropdown-menu-wrap--open');
+    }, 50);
 });
 
